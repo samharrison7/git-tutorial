ea6b15a0022cf95f93e1a9a6c235da9361539d84
Sam Harrison <samharrison.xg@gmail.com>
2023-02-27 20:32:23 +0000

    Adding the time to output

--- hello_world.f90
+++ hello_world.f90
@@ -2,9 +2,11 @@ program hello_world
     implicit none
 
     character(8) :: date
+    character(10) :: time
 
-    call date_and_time(date=date)
+    call date_and_time(date=date, time=time)
     
     print *, "Hello world!"
-    print *, "The date is", date
+    print *, "The date is ", date
+    print *, "The time is ", time
 end program hello_world
