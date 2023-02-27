326d277ff8f435e7cacae2fe168a2d9e3934d6c9
Sam Harrison <samharrison.xg@gmail.com>
2023-02-27 20:30:18 +0000

    Outputting today's date

--- hello_world.f90
+++ hello_world.f90
@@ -1,5 +1,10 @@
 program hello_world
     implicit none
+
+    character(8) :: date
+
+    call date_and_time(date=date)
     
     print *, "Hello world!"
+    print *, "The date is", date
 end program hello_world
